ef774ebcc7ccf708ea7ce6f4eb179067b307cbd9
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-12-23 09:18:47 -0500

    Consulta de emisiones prioridad fecha

---
+++ consultas/emisiones.sql
@@ -0,0 +1,33 @@
+WITH leads_ordenados AS (
+  SELECT 
+    KEY_ID,
+    TIPO_DOCUMENTO,
+    FECHA_LEAD,
+    FUENTE_LEAD,
+    MEDIO_LEAD,
+    CAMPANA_LEAD,
+    PRODUCTO_SELECCIONADO,
+    NOMBRE_FORMULARIO,
+    ROW_NUMBER() OVER(
+      PARTITION BY KEY_ID 
+      ORDER BY FECHA_LEAD DESC
+    ) as ranking_reciente,
+    COUNT(*) OVER(PARTITION BY KEY_ID) as total_contactos
+  FROM `sb-ecosistemaanalitico-lago.contact_center.t_seguimiento_leads_mercadeo`
+  WHERE PRODUCTO_SELECCIONADO = 'PROTECCION DE CREDITOS'
+    AND KEY_ID IS NOT NULL
+)
+
+SELECT 
+  KEY_ID AS identificador,
+  TIPO_DOCUMENTO,
+  FECHA_LEAD AS fecha_contacto_mas_reciente,
+  FUENTE_LEAD,
+  MEDIO_LEAD,
+  CAMPANA_LEAD,
+  PRODUCTO_SELECCIONADO,
+  NOMBRE_FORMULARIO,
+  total_contactos
+FROM leads_ordenados
+WHERE ranking_reciente = 1 
+ORDER BY FECHA_LEAD DESC
